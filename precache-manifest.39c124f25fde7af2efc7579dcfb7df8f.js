3cebb2b0ff76d2ef91ed3072d41f7b1f1e4a966e
fegzycole <fergusoniyara@gmail.com>
2020-02-19 17:29:11 +0100

    Updates

---
+++ precache-manifest.39c124f25fde7af2efc7579dcfb7df8f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8b74ddf50959d447a08098091f3c8c",
+    "url": "/Ferguson-Iyara/index.html"
+  },
+  {
+    "revision": "eb0ab29fb3d1628d1ed1",
+    "url": "/Ferguson-Iyara/static/css/main.40b9fe62.chunk.css"
+  },
+  {
+    "revision": "5892cfb873a1785ec0eb",
+    "url": "/Ferguson-Iyara/static/js/2.06312d88.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Ferguson-Iyara/static/js/2.06312d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0ab29fb3d1628d1ed1",
+    "url": "/Ferguson-Iyara/static/js/main.28f1f8e5.chunk.js"
+  },
+  {
+    "revision": "c8c60d9f23ae629c721f",
+    "url": "/Ferguson-Iyara/static/js/runtime-main.7a7a9ab6.js"
+  },
+  {
+    "revision": "0c4ada3075b70137ea2716e64e974751",
+    "url": "/Ferguson-Iyara/static/media/Ferguson-Iyara.0c4ada30.pdf"
+  },
+  {
+    "revision": "6fb1eb9e884da9e9400d25df11336af0",
+    "url": "/Ferguson-Iyara/static/media/camphor-bold.6fb1eb9e.woff"
+  },
+  {
+    "revision": "8f5bae067cef77be999a6120bd716828",
+    "url": "/Ferguson-Iyara/static/media/camphor-light.8f5bae06.woff"
+  },
+  {
+    "revision": "426cf0da281f0a6326f26ca080be28d2",
+    "url": "/Ferguson-Iyara/static/media/camphor-medium.426cf0da.woff"
+  },
+  {
+    "revision": "cdd982e68427f60ae5d4d33701acb68f",
+    "url": "/Ferguson-Iyara/static/media/camphor.cdd982e6.woff"
+  },
+  {
+    "revision": "179f96cc364e3710a261af93c058f128",
+    "url": "/Ferguson-Iyara/static/media/monaco-webfont.179f96cc.179f96cc.woff"
+  },
+  {
+    "revision": "6bb89fb6a34ff192acd4ccba0d771e3b",
+    "url": "/Ferguson-Iyara/static/media/myBanka.6bb89fb6.png"
+  },
+  {
+    "revision": "64ca4013d7511b490e99a2e201810528",
+    "url": "/Ferguson-Iyara/static/media/myFacebook.64ca4013.png"
+  },
+  {
+    "revision": "891c22d21e6c23d122f2f5e6d166ecd4",
+    "url": "/Ferguson-Iyara/static/media/myTwitter.891c22d2.png"
+  }
+]);
